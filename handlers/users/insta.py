6c36d1b9995d916655bcdb9e93ea4022569d2440
Sarvarbek <104064513+Sarvarbek0316@users.noreply.github.com>
2022-06-21 01:20:58 +0500

    Add files via upload

--- handlers/users/insta.py
+++ handlers/users/insta.py
@@ -1,10 +1,18 @@
 from aiogram import types
 from loader import dp
-from main import instag, tiktok
+from main import instag, tiktok, youtube
 
 @dp.message_handler(content_types='text')
 async def test(ms:types.Message):
     link=ms.text
+    
+    try:
+        result = youtube(link)
+        for i in result:
+            if i['hasAudio']:
+                await ms.answer_video(i['url'])
+    except:
+        pass
     
     try:
         result = tiktok(link)
